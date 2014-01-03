b904e6ba3b45e01ed3b7997fc564b0b9a693e0f7
AlexJPZ17 <alexjpz17@gmail.com>
2014-01-02 22:46:32 -0600

    VB Multiplicacion y raiz cudrada

---
+++ Multiplicacion y raiz cudrada/Module1.vb
@@ -0,0 +1,22 @@
+﻿Module Module1
+
+    Sub Main()
+        'Creamos  5 variables de tipo double
+        Dim a, b, c, d, e As Double
+        'Cramos un mensaje
+        Console.WriteLine("Introduzca Una Variable ")
+        a = Double.Parse(Console.ReadLine())
+        Console.WriteLine("Introduzca La Segunda Variable")
+        b = Double.Parse(Console.ReadLine())
+        c = a * b
+        Console.WriteLine("El Producto de las variables es: " & c)
+        d = c * c * c
+        Console.WriteLine("El Cubo del producto es: " & d)
+        'Saca la raiz cuadrada
+        e = Math.Sqrt(d)
+        Console.WriteLine("La Raiz Cuadrada Del Cubo es: " & e)
+        Console.ReadLine()
+
+    End Sub
+
+End Module
---
+++ Multiplicacion y raiz cudrada/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general sobre un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos atributos para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("Multiplicacion y raiz cudrada")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("Multiplicacion y raiz cudrada")> 
+<Assembly: AssemblyCopyright("Copyright ©  2013")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como identificador de typelib si este proyecto se expone a COM
+<Assembly: Guid("4bd7a30c-ccc8-470c-990e-2facdf76cbf4")> 
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria 
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión 
+' mediante el asterisco ('*'), como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
